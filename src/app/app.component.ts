62f8bca75807d0f29dc335f3f0a4e40ca1065bc5
Cesar <cesar.a.cortes.94@gmail.com>
2023-11-12 14:15:59 -0600

    Primer commier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServiceService } from './service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-aws-docker';
+  resultados: any[] = [];
+
+  constructor(private servicio: ServiceService) {}
+
+  ngOnInit(): void {
+    this.servicio.getResultados().subscribe(data => {
+      this.resultados = data;
+    });
+  }
 }
